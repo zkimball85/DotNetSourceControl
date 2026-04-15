0bcc2033902450ced2458005a817ff4acb7bb512
Zac Kimball <z.kimball85@gmail.com>
2026-04-15 13:29:13 -0700

    Add button1 to Form1 and reformat Form1.resx XML
    Added a new Button control (button1) to Form1, including initialization, positioning, sizing, and adding it to the form's Controls. Also reformatted Form1.resx for improved XML readability; no resource content was changed.

--- DotNetSourceControl/Form1.Designer.cs
+++ DotNetSourceControl/Form1.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(272, 172);
+            button1.Name = "button1";
+            button1.Size = new Size(188, 76);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
